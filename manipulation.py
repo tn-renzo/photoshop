4a146616a61f0df7f17507c72f56c7eadf5952f6
tn-renzo <lorenz_tanner@icloud.com>
2026-05-11 18:20:01 +0200

    Update error handling in manipulation.py

--- manipulation.py
+++ manipulation.py
@@ -2,6 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
+"""
+Notes:
+    Numpy array: array[:, :, xxx]
+     - channel 0 ... red
+     - channel 1 ... green
+     - channel 2 ... blue
+"""
+
 def load_image(image_path) -> np.ndarray:
     img = Image.open(image_path)
     return np.array(img)
@@ -12,31 +20,26 @@ def grayscale(image):
 def invert(image):
     pass
 
-"""
-    Numpy array: array[:, :, xxx]
-     - channel 0 ... red
-     - channel 1 ... green
-     - channel 2 ... blue
-"""
 def set_green(image: np.ndarray, rgb_value: int) -> np.ndarray:
-    if rgb_value < 0 or rgb_value < 255:
+    if rgb_value < 0 or rgb_value > 255:
         print("Invalid RGB Value")
-        # TODO: Add a return type or try/except for this
-
+        return image # Early unmodified return
 
     image[:, :, 1] = rgb_value
     return image
 
 def set_red(image: np.ndarray, rgb_value: int) -> np.ndarray:
-    if rgb_value < 0 or rgb_value < 255:
+    if rgb_value < 0 or rgb_value > 255:
         print("Invalid RGB Value")
+        return image
 
     image[:, : , 0] = rgb_value
     return image
 
 def set_blue(image: np.ndarray, rgb_value: int) -> np.ndarray:
-    if rgb_value < 0 or rgb_value < 255:
+    if rgb_value < 0 or rgb_value > 255:
         print("Invalid RGB Value")
+        return image
 
     image[:, :, 2] = rgb_value
     return image
